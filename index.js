7f99bca107e5b881f26c3b1a217d611417094aad
Juraj Moudrý <xmoudry@fi.muni.cz>
2021-07-30 21:25:46 +0200

    dlho som nic nesave-ol :)

--- index.js
+++ index.js
@@ -1,19 +1,15 @@
-class Receipt {
-
-    Receipt (name, lastName, product, storage) {
-        this.name = name;
-        this.lastName = lastName;
-        this.product = product;
-        this.storage = storage;
-        this.price = null;
-    }
-
-    addPrice (price) {
-        this.price = price;
-    }
-
-}
-
-function chaos () {
-    document.getElementsByClassName('para').style.fontSize = '10px';
+function write() {
+    var firstN = document.getElementById(firstName);
+    var lastN = document.getElementById(lastName);
+    var prod = document.getElementById(product);
+    var stor = document.getElementById(storage);
+    var pri = document.getElementById(price);
+    const rec = {
+        firstName: firstN.options.value, 
+        lastName: lastN.options.value, 
+        product: prod.options[prod.selectedIndex].value,
+        storage: stor.options[stor.selectedIndex].value,
+        price: pri.options.value
+    };
+    window.location.replace('finish.html');
 }
